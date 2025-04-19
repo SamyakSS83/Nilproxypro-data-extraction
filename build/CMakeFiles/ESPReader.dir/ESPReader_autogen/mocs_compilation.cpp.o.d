b8c2452d2b5dd095f07d37c6cedf0e7531e74b0a
SamyakSS83 <cs1230807@iitd.ac.in>
2025-04-19 19:38:59 +0530

    clean up

--- build/CMakeFiles/ESPReader.dir/ESPReader_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ESPReader.dir/ESPReader_autogen/mocs_compilation.cpp.o: \
- /home/threesamyak/biometric/esp_reader/build/ESPReader_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
